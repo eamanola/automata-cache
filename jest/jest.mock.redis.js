92f3e8934ff1196ba265f2012f1e48bdf8855ba6
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-05-10 20:02:18 +0300

    allow real server for testing

--- jest/jest.mock.redis.js
+++ jest/jest.mock.redis.js
@@ -1,26 +1,34 @@
 // To test against redis server, see backend-empty
 // out of scope of this package
+const { REDIS_URL } = require('../src/config');
 
-jest.mock('redis', () => {
-  const cache = {};
+if (REDIS_URL === 'use-mock') {
+  console.info('mocking redis');
 
-  const redis = {
-    createClient: () => ({
-      connect: () => null,
-      del: async (key) => {
-        if (typeof key === 'string') {
-          delete cache[key];
-        } else if (Array.isArray(key)) {
-          key.forEach((k) => redis.createClient().del(k));
-        }
-      },
-      disconnect: () => null,
-      get: async (key) => cache[key],
-      set: async (key, value) => {
-        cache[key] = value;
-      },
-    }),
-  };
+  jest.mock('redis', () => {
+    const cache = {};
 
-  return redis;
-});
+    const redis = {
+      createClient: () => ({
+        connect: () => null,
+        del: async (key) => {
+          if (typeof key === 'string') {
+            delete cache[key];
+          } else if (Array.isArray(key)) {
+            key.forEach((k) => redis.createClient().del(k));
+          }
+        },
+        disconnect: () => null,
+        get: async (key) => cache[key],
+        set: async (key, value) => {
+          cache[key] = value;
+        },
+      }),
+    };
+
+    return redis;
+  });
+} else {
+  console.info(`skipping mock. make sure redis server is running and REDIS_TEST_URL is set.
+to use mock set REDIS_TEST_URL=use-mock`);
+}
